672f6154f65a00500eb6ffe5d057436025d95700
AngelNetwork Corner <161264650+Amwmiller5@users.noreply.github.com>
2024-08-02 06:48:38 -0400

    Create jest.config.ts

---
+++ nx/packages/devkit/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  globals: {},
+  displayName: 'AngelNetCoin_Mainnet_App',
+  preset: '../../jest.preset.js',
+};
