00920fc8d5182b3ba595ab29fa60f77a26164ae4
AngelNetwork Corner <161264650+Amwmiller5@users.noreply.github.com>
2024-08-02 06:52:51 -0400

    Create ngANCNetwork-adapter.ts

---
+++ nx/packages/devkit/ngANCNetwork-adapter.ts
@@ -0,0 +1,9 @@
+/**
+ * @category AngelNetCoin Mainnet App (CryptoNetwork.io)
+ */
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+export {
+  wrapAngularDevkitSchematic,
+  mockSchematicsForTesting,
+  NxScopedHost,
+} from 'nx/src/adapter/ngcli-adapter';
