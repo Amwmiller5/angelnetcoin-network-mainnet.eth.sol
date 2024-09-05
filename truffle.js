7db66de73c713fcd1ec7b12da2a0909d58321b8f
AngelNetwork Corner <161264650+Amwmiller5@users.noreply.github.com>
2024-09-05 13:48:45 -0400

    Create truffle.js
    Signed-off-by: AngelNetwork Corner <161264650+Amwmiller5@users.noreply.github.com>

---
+++ truffle.js
@@ -0,0 +1,368 @@
+ganche-ui
+ganacge-plugin-filecoin
+client: http://127.0.0.1:7545
+
+npm install -g truffle
+
+truffle develop
+client: http:// 127.0.0.1:9545
+
+[deploy to public networks]
+
+geth(go-ethereum):https://github.com/ethereum/go-ethereum
+
+WebThree(cpp-ethereum):https://github.com/ethereum/cpp-ethereum
+
+hyperledger Besu(java):https://github.com/hyperledger/besu
+
+AngelNetCoin(AngelNetCoin-Network):https://github.com/Amwmiller5/ANC_MAINNET_APP.sol
+
+[externalCommand]
+
+module.exports= {
+     //This runs the 'webpack' on each build
+    //
+    //WORKING_DIRECTORY: 'https://github.com/Amwmiller5/ANC_MAINNET_APP.sol'
+
+  //BUILD_DESTINATION_DIRECTORY: ''https://github.com/Amwmiller5/ANC_MAINNET_APP.sol
+  build: 'webpack'
+
+    [custom functions]
+
+    module.exports= {
+   build:functions(options, callbacks) {
+       sync: "https://github.com/Amwmiller/ANC_MAINNET_APP.sol"
+         compile: true
+         DeployToGanacheBlockchain: true
+         post: "https://go.getblock.io/b80711db4a99407ea1fc108303b4b855"
+         merge: true
+      sync: "https://angelnetworknftcollections.unicornplatform.page/meet-our-angelnetworkcorner-angels/?noca"
+}
+}
+
+fork: {
+     'https//blockscan.com',
+     'https://etherscan.io',
+     'https://hardhat.org',
+     'https://smartcontracts.tools',
+     'https://metamask.io',
+     'https://spydra.app'
+     'https://optimisom.io'
+     'https://vscode.blockscan.com/ethereum'
+     'https://tokentool.bitbond.com'
+     'https://binance.com'
+     'https://opensea.io/collection/angelnetcoin_nft'
+     'https://phosphor.xyz/userId/f253fd87-afc1-46d4-bdf0-bc222d1b1153'
+     'https://alchemy.com/apiKey/1d2wQZEMUm8-KWWk095XYALWyNIRHS3-0
+     'https://alchemy.com/gasPolicy/633254b8-5cbd-4cf7-b236-869a1c0bddcc'
+     'https://manage.0auth.com/CBD30X0tvUiQAbKBKbLQzhSKcKK5r1sy'
+     'https://mintme.host/panel'
+       'https://AngelNetCoin.mintme.host'
+      'https://platform.openai.com/api-keys/AngelNetCoin_BTC/sk-proj-IZEd7fGQAujE0NdLu378T3B1bkFJz0ai0mv7m2XgiFnbAuGN'
+      'https://dune.com/B3YIvjHTcw6hKAM1haGJYyrDaDLxuWN0'
+       'https://AngelNetworkCornerNFTCustomerCarebot.zapier.app
+}
+}
+[customModule]
+varDefaultBuilder= require("truffle-default-builder");
+module.exports= {
+  Build: ("AngelNetCoin_Network_Blockchain_App")
+    config {
+  EtherscanRegistration: (
+    registeredName "angelnetcoin-mainnet.eth"
+    registrationTxnHash = 0x6d89be64e5ea0865ed33d601d77c109e51e84f677d489e3a0a212a25c2ea1eb0
+    Status= "success"
+    Block= "6629655" 108 block confirmations
+    TimeStamp= "Sep-4-2024 05:55:12AM"
+    TransactionAction: ("CallRegisterFunctionUTC")
+    by '0xEd70927F4E2Cdd3000cd20Db57fFe2E3e9683159' on 'oxFED6a969AaA60E4961FCD3EBF1A2e8913ac65B72'
+    ERC721Token= tokenID["111879087135359831264757086486781725906037121426504056453976869582455648825465"]('https://sepolia.etherscan.io/token/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85')
+    ERC-1155TokenId: ["71833854614384107859402753184467276052253728385201902253699577321567407366477"](NameWrapper('0x0635513f179d50a207757e05759cbd106d7dfce8'))
+    value: "0.000261986301370155ETH"
+    transFee: "0.017766592d54241677ETH"
+    Gas Price: "6.693692313Gwei"
+    Atributes: ("txnType:2(EIP-1559)Nonce:23 Position in Block: 86")
+    Data: "0xddf252ad1be2c89b69c2068fc378daa952ba7f163c4a11628f55a4df523b3ef" , "0x6A79596f19b33B5f42b597E1b1595ccBDb5c00Fa"
+      appENS: "app.ens.domains/angelnetcoin-mainnet.eth/register" 
+        contractRegisterAddress: "0x0635513f179D50A207757E05759CbD106d7dFcE8";
+}
+}
+[bootStrapApp]
+
+{putAllArtifacts inBuildPipeline}./build/contractsDirectory: ".jsonFiles", "https:github.com/Amwmiller5/ANC_MAINNET.sol" , "AllForkLinksAbove";
+HTTPProvider= "alpha-spring-film.ethereum-sepolia-quicknode.pro/0de5a966be2f6e5bbd7c879358208e0fb2024fde1"
+turn .json contract artifacts into contract abstractions that are easy to use
+@truffle/contract
+addUserWalletProvider forAngelNetCoinWallet {
+  Provider= "https://coinbase.com" , "ProjectId: c062ba7e-1543-4922-b0e1-64fc014e20f5" , "https://api.developer.coinbase.com/rpc/v1/base/KFWbz0X6pJLsM0cV517otj0cGl5Wiip" , "ApiKey: organizations/9fbbdab9-2d5a-46ba-a8b3-04441a87a196/apiKeys/7cd480c2-04f1-4650-9190-69ab92e83306"
+  Provider2= "https://withered-maximum-season.bsc.quicknode.pro/0aa31fae20d7ac319dc5e6d244df946678d387f6/"
+ syncAppProvider forAngelNetCoinNetworkBlockChainApp {
+   Provider= "@alchemy/Angelnetcoin_Mainnet.appId/gxeidu98rfq7fh8z/AppKey/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   ethereum= "https://eth-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   Polygon= "https://polygon-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   Arbitrum= "https://arb-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   optimisum= "https://opt-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   polygonzkEvM= "https://polygonxkeum-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   starknet v0-7= "https://starknet-mainnet.g.alchemy.com/starknet/version/rpc/v0_7/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   Solana= "https://solana-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   Astar= "https://astar-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   ZKsync= "https://zksync-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   Zora= "https://zora-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   Frax= "https://frax-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   zetachain= "https://zetachain-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   blast= "https://blast-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+   metis= "https://metis-mainnet.g.alchemy.com/v2/3GbqLIT8rB-Lo0PTRKiKda20kgfkvo8z"
+ }
+  [quicknodeSDK]
+  npm install @quicknode/sdk-getblock
+
+   constQuicknode=require('@quicknode/sdk');
+  importQuicknode from'@quicknode/sdk' ;
+
+  constructCore= newCore ({
+    endpointURL:'https://withered-maximum-season.bsc.quicknode.pro/0aa31fae20d7ac319dc5e6d244df946678d387f6/;
+  })
+    constCurrentBlockNumber= awaitCore.client.getBlockNumber();
+      console.log(currentBlockNumber)
+
+  MevProtection&privateReinforcedTransactions-eth_sendRawTransaction from web3 importWeb3
+    w3=
+      Web3(web3.HTTPProvider('https://withered-maximum-season.bsc.quicknode.pro/0aa31fae20d7ac319dc5e6d244df946678d387f6'))
+       resp-
+         w3.provider.make_request('eth_sendzrawTransaction;
+                                  [^_tranaction#1_sendETH_from"0x0000000000000000000000000000000000000000"_to_"0xEd70927F4E2Cdd3000cd20Db57fFe2E3e9683159"_uint256="$40e18ETH"]ApproveTransaction
+                     Transaction Complete= true
+                         Print(resp)
+                           PublicReturn GetTxnHash= true
+  varjason= require("./build/contracts/AngelNetCoin_Network_Blockchain.json");
+    varAngelNetCoin_Network_Blockchain= contract(json);
+
+  AngelNetCoin_Network_Blockchain.setProvider(newWeb3.provider.HTTPProvider)
+
+  AngelNetCoin_Network_Blockchain.deployed().then("function(Deployed)"{
+                                                  returnDeployed.AngelNetCoin_Network_Blockchain_App();
+  });
+}
+}
+truffle develop
+'https://127.0.019545'
+   compile
+--networkName<angelnetcoin-network-blockchain-mainnet.eth>
+
+  configuration
+truffle-config.js
+  module.exports= {
+    networks: {
+      Development: {
+        host: "127.0.0.1",
+        port: 8545 , 7545
+          network_id "5777" , "1"
+          "http://0.0.0.0:7545"
+          hardFork: "London"
+          AutoMinning= true
+}
+}
+
+compilers: {
+  solc: {
+    version: "^0.8.0"
+  }
+}
+};
+console.log
+
+pragma solidity>=0.4.25<0.9.0;
+importTruffle/console.sol";
+
+module.exports= {
+  ...
+    solidityLog: {
+    displayPrefix: ":ANC"
+}
+}
+  $truffleTest
+compiling./contracts/ANC_MAINNET_APP.sol@github.com/Amwmiller5
+module.exports= {
+  ...
+    SolidityLog:
+      preventConsoleLogMigration: true,
+  }
+} net_version
+TruffleMigrate--AngelNetCoinNetwork--network live
+  Network_Id: 5777
+ gas: 6721975
+gas price: 20000000000
+from: 0x34f0130b19835bEd995Ecc2A56615C10DE7c7C31
+provider: "new"
+>newWeb3Provider= "AngelNetCoin Network Mainnet" , "angelnetcoin-mainnet.eth"
+web3.providersHTTPProvider("https://127.0.0.1:7545");
+webSockets: .on
+deploymentPollingInterval: 4000
+forTruffle {
+   "https://127.0.0.1:8545",
+  "ws://127.0.0.1:8545",
+     "https://sandbox.truffleteams.com/.."
+     "wss://sandbox.truffleteams.com/..."
+
+  config.networks.AngelNetCoin_Network.registry.address
+    AngelNetCoin_Network.ens.registry.0xEd70927F4E2Cdd3000cd20Db57fFe2E3e9683159
+      package_Name="AngelNetCoin_Network_Mainnet",
+      version: "0.0.3"
+      "@angelnetcoin-mainnet.eth"
+      authors: [
+        "AngelNetwork_Corner(Founder:Angela_Williams)<the.angelnetworkcorner@gmail.com>"
+        ]
+  keywords: [
+    "ethereum"
+    "Network"
+    "Crypto"
+    "NFT Marketplace"
+    "Marketplace"
+    "Earn_Free_Crypto"
+    "Support_NonProfits"
+    "AngelNetwork"
+    "ANC"
+    "AngelNetCoin"
+    "Blockchain"
+    "Mainnet"
+    "angelnetcoin-mainnet.eth"
+    "eth, ETH"
+    "Drawings"
+    "Contests"
+    "win_Real_Crypto"
+    "Ways_to_earn"
+    "staking"
+    "loans"
+    "AirDropps"
+    "provider"
+    "cryptocurrency"
+    "The_Best_Network"
+    "Ways_to_Give_Back"
+    "Donating"
+    "Direct_Donations"
+    "Raffles"
+    "Crypto_Bank"
+    "developers"
+    "wallets"
+    "NFTs"
+    "Network"
+    "Crypto_Merchandise"
+    "Crypto_Backed_Loans"
+    "Easy"
+    "fast"
+    "Secure"
+    "Web3"
+    "decentralized"
+    "Apps"
+    "available_jobs"
+    "business_professionals"
+    "Web_Building"
+    "Contributing"
+    "Creating"
+    "Art_Collections"
+    "Collections"
+    "Contracts"
+    "Blocks"
+    "Help"
+    "Guidance"
+    "Angels"
+    "Angel_Investing"
+    "Nonprofits"
+    "Worldwide"
+    "International"
+    "United_States"
+    "USA"
+    "Assistance"
+    "Grants"
+    "Research_Help"
+  dependencies: {
+    "Owned": "^0.0.1"
+    "erc-115"
+    "erc20-token": "^1.0.0"
+    "erc721"
+license: "MIT"
+  }
+}
+merge:"https://github.com/Amwmiller5/ANC_MAINNET_APP.sol
+  mnemonic, "HTTP:" "winner interest damp wish mansion mention kit desk dress potato jungle million"
+
+module.exports= {
+
+  migrations_contracts_directory: "https://github.com/Amwmiller5/ANC_MAINNET_APP.sol"
+    networks: {
+  "AngelNetCoin_Network_Blockchain@angelnetcoin-mainnet.eth"
+
+      Development {
+        host: "127.0.0.1",
+        port: 8545, 7545,
+        networkID: "1",
+          ANCNetworkId: "5777" 
+        ownerAddress: "0xEd70927F4E2Cdd3000cd20Db57fFe2E3e9683159"
+      }
+}
+}
+module.exports= {
+  dashboard: {
+    port: 24012
+      host: "localhost"
+      verbose: true
+}
+}
+etherscan:
+   apiKey: "7RJKCEEXEHBJAD8EJ6VSW76NJKP5321"
+}
+sourceFetchers: ["etherscan","sourcify"]
+}
+}
+module.exports={
+  import "https://github.com/Amwmiller/ANC_MAINNET_APP.sol"
+ens: {
+  enabled: true
+  registry: {
+    address: "0xEd70927F4E2Cdd3000cd20Db57fFe2E3e9683159"
+  }
+}
+npm install -g truffle
+  truffle unbox rsksmart/rsk-react-box
+
+truffle develop
+
+compile: true
+migrate: true
+        inClient Directory
+cd client
+npm run start
+  <client/build folder>
+  npm run build
+}
+}
+"https://dev.rootstock.io/dev-tools/wallets
+  BIP-44
+Mainnet: "m/44'/137'/0'/0/N
+  testnet: "m/44'/37310'/0'/0/N
+  Import EIP-1191 "@openzeppelin/contracts/EIP_1191/EIP1191
+  Import EIP-155 "@openzeppelin/contracts/EIP-155/EIP155
+
+
+        
+      
+
+
+
+
+  
+
+
+          
+   
+
+
+
+  
+                                                                                                           
+                                                                                                           
+                                                                                                           
+                                                                                                           
+                                                                                                           
+                                                                                                           
+    
